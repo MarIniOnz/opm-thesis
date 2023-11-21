b150c5087c5ab553ea0853058ffe542cc595e281
MarIniOnz <martin.iniguez@limebit.de>
2023-11-21 14:44:15 +0100

    small changes

--- opm_thesis/create_epochs/epochs_hilbert_bands.py
+++ opm_thesis/create_epochs/epochs_hilbert_bands.py
@@ -4,12 +4,10 @@ This script is used to create epochs in different frequency bands using the
 Hilbert transform. The epochs are saved in different files for each frequency."""
 import pickle
 import mne
+import numpy as np
 
 
-DATA_SAVE = (
-    r"C:\Users\user\Desktop\MasterThesis\opm-thesis\data\data_nottingham_preprocessed"
-)
-
+DATA_SAVE = "./data/"
 all_bads = []
 acq_times = ["155445", "160513", "161344", "163001"]
 
@@ -20,41 +18,54 @@ low_mid_gamma = dict({"l_freq": 30, "h_freq": 45})
 mid_gamma = dict({"l_freq": 45, "h_freq": 75})
 high_gamma = dict({"l_freq": 60, "h_freq": 120})
 all_gamma = dict({"l_freq": 30, "h_freq": 120})
+mid_beta = dict({"l_freq": 15, "h_freq": 22})
 
 frequencies = {
     "alpha": alpha,
     "beta": beta,
     "low_gamma": low_gamma,
-    "low_mid_gamma": low_mid_gamma,
-    "mid_gamma": mid_gamma,
+    # "low_mid_gamma": low_mid_gamma,
+    # "mid_gamma": mid_gamma,
     "high_gamma": high_gamma,
-    "all_gamma": all_gamma,
+    # "all_gamma": all_gamma,
+    "mid_beta": mid_beta,
 }
 
 for key, frequency_params in frequencies.items():
     preprocessed_epochs = []
+    baseline = []
 
     for acq_idx, acq_time in enumerate(acq_times):
-        with open(DATA_SAVE + "\\preprocessing_" + acq_time + ".pkl", "rb") as f:
+        with open(
+            DATA_SAVE
+            + "data_nottingham_preprocessed/preprocessing_"
+            + acq_time
+            + ".pkl",
+            "rb",
+        ) as f:
             preprocessing = pickle.load(f)
 
         raw_filtered = preprocessing.apply_filters(
-            preprocessing.raw,
+            preprocessing.raw_corrected,
             frequency_params,
             notch_filter=False,
         )
         picks = mne.pick_types(raw_filtered.info, meg="mag", exclude="bads")
         hilbert_transformed = raw_filtered.copy().apply_hilbert(picks=picks)
         hilbert_epochs = preprocessing.create_epochs(hilbert_transformed)
+        baseline.append(hilbert_epochs.baseline[-1])
 
         preprocessed_epochs.append(hilbert_epochs)
         all_bads.extend(hilbert_epochs.info["bads"])
 
+    baseline = np.mean(baseline)
     for acq_idx, epoch in enumerate(preprocessed_epochs):
+        epoch.apply_baseline(baseline=(-2, baseline))
         epoch.info["bads"] = all_bads
+        epoch.drop_bad()
 
     all_epochs = mne.concatenate_epochs(preprocessed_epochs)
-    file_name = DATA_SAVE + "\\epochs\\hilbert_" + key + "_all_epochs.pkl"
+    file_name = DATA_SAVE + "epochs/hilbert_" + key + "_all_epochs.pkl"
 
     with open(file_name, "wb") as f:
         pickle.dump(all_epochs, f)
